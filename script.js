05a38663197e64ed9a38f54f67a5f2e26e4ecc96
Zandakumsha <zandaj.kumsha@gmail.com>
2026-05-27 12:06:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// This script demonstrates the use of for and while loops to iterate through an array of numbers.
+//l need help with creating a loop that prints only even numbers from the numbers array.
+// Can Karah please help me with that?
+const numbers = [1, 2, 3, 4, 5];
+
+console.log("Using for loop:");
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
+
+console.log("Using while loop:");
+while (numbers.length > 0) {
+  let number = numbers.pop();
+  console.log(number);
+}
