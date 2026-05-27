d0a0dce5ce67c184df7d11dca6ab29fd2714aaec
Zandakumsha <zandaj.kumsha@gmail.com>
2026-05-27 12:39:02 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,8 +1,16 @@
+// Karah: I've created a function called greet that takes a name parameter and I've made sure that the code will be able to print a greeting message to the console when the function is called with a name argument.
+// I need help with creating a function called add that takes two parameters and makes the add function return the sum of both parameters.
+//Zanda do mind helping me?
 function greet (name){
   return "Hello, " + name + "!";
 }
-// Karah: I've created a function called greet that takes a name parameter and I've made sure that the code will be able to print a greeting message to the console when the function is called with a name argument.
-// I need help with creating a function called add that takes two parameters and makes the add function return the sum of both parameters.
+// No problem KaraH I've got you.
+//Zanda here. 
+//I will be helping Karah wth creating a function called add that takes two parameters and makes the add function return the sum of both parameters.
+function add(a, b){
+  return a+b;
+}
 
 console.log(greeting);
 console.log(sum);
+
